a7d3092c1adb2798dbfe5715998db49073a5bebe
Daniele Orler <daniele.orler@gmail.com>
2018-09-25 21:35:51 +0200

    development environment

---
+++ wp-discount-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*
+    Plugin Name: Wordpress Discount Plugin
+    Plugin URI: https://github.com/claudiazanetel/wp-discount-plugin
+    Version: 0.0.1
+    Author: Claudia Zanetel
+    Description: Manage discount vouchers
+    Text Domain: wp-discount-plugin
+    License: MIT
+    */
